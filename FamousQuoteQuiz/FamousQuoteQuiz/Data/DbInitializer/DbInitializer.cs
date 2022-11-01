87f838260e71f32bfb9ffa3cb30dbab177f524a9
Nikolay Ivanov <niki_8905@abv.bg>
2022-11-01 03:00:30 +0200

    Add default data and implement dbInitializer

--- FamousQuoteQuiz/FamousQuoteQuiz/Data/DbInitializer/DbInitializer.cs
+++ FamousQuoteQuiz/FamousQuoteQuiz/Data/DbInitializer/DbInitializer.cs
@@ -1,27 +1,60 @@
 ﻿namespace FamousQuoteQuiz.Data.DbInitializer
 {
     using FamousQuoteQuiz.Data.EntityModels;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
 
     public static class DbInitializer
     {
         public static void Initialize(this FamousQuoteQuizDbContext context)
         {
             context.Database.EnsureCreated();
-
-            //TEST IF EF CORE WORK!
-            var author = new Author()
+            //if (context.Authors.Any() == true)
+            //{
+            //    return;
+            //}
+            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string authorsPath = Path.Combine(baseDirectory, "..\\..\\..\\Data\\DbInitializer\\Authors.json");
+            string authorJson = File.ReadAllText(authorsPath);
+            if (string.IsNullOrEmpty(authorJson) == false)
             {
-                Name = "Test entityFramework"
-            };
-            
-            context.Authors.Add(author);
-            context.SaveChanges();
+                var authors = JsonConvert.DeserializeObject<dynamic>(authorJson);
+                foreach (var author in authors)
+                {
+                    var dbAuthor = new Author()
+                    {
+                        Name = author.Name
+                    };
 
-            //TODO: Initialize data!!!
-            if (context.Authors.Any() == true)
-            {
-                return;
+                    context.Authors.Add(dbAuthor);
+                }
+
+                context.SaveChanges();
             }
+
+            string quotesPath = Path.Combine(baseDirectory, "..\\..\\..\\Data\\DbInitializer\\Quotes.json");
+            string quotesJson = File.ReadAllText(quotesPath);
+            if (string.IsNullOrEmpty(quotesJson) == false)
+            {
+                var quotes = JsonConvert.DeserializeObject<dynamic>(quotesJson);
+                foreach (var quote in quotes)
+                {
+                    int? authorId = quote.AuthorID;
+                    if (authorId != null)
+                    {
+                        var author = context.Authors.Where(author => author.Id == authorId).FirstOrDefault();
+                        string quoteContet = quote.Content;
+                        var dbQuote = new Quote()
+                        {
+                            Author = author,
+                            Content = quoteContet.Length <= 50 ? quoteContet : quoteContet.Substring(0, 50)
+                    };
+
+                    context.Quotes.Add(dbQuote);
+                }
+            }
+            context.SaveChanges();
+        }
         }
     }
 }
